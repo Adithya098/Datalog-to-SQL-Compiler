faa671c8f884624c7a1e51c4f5dda1cb6a21e65c
Jodi Oo <bolshack_fire@hotmail.com>
2024-03-30 18:47:37 +0800

    Clean up the main class slightly

--- datalog_compiler/src/main.py
+++ datalog_compiler/src/main.py
@@ -43,7 +43,6 @@ def sql_connection(output_file, database="datalog_to_sql_db", username="postgres
 
 
 def generate_sql_query_from_datalog_query(datalog_query, interpreter=None):
-    # print (sys.path)
     if not interpreter:
         interpreter = Interpreter()
     ast = parser.parse(datalog_query)
@@ -78,34 +77,11 @@ if __name__ == "__main__":
 
     interpreter = Interpreter()
 
-    # sql_handler = sql_connection(output_file)
-    # database = "datalog_compiler"
-    # username = "postgres"
-    # pwd = "postgres"
-    # port_id = 5432
-
-    # try:
-    #     conn = psycopg2.connect(dbname=database, user=username, password=pwd, port=port_id)
-    # except psycopg2.Error as e:
-    #     print("Unable to connect to the database:", e)
-    #     sys.exit(1)
-
-    # sql_handler = SQLHandler(conn)
-    # sql_handler.drop_all_objects()
-
-    # file_handler = SQLFileHandler(sql_handler, output_file)
-    # observer = Observer()
-    # observer.schedule(file_handler, os.path.dirname(os.path.abspath(output_file)), recursive=False)
-    # observer.start()
-
     while True:
         try:
             if isREPL:
                 datalog_query = input('What is the datalog query?\n')
                 sql_queries = generate_sql_query_from_datalog_query(datalog_query, interpreter)
-                for sql_query in sql_queries:
-                    sql_handler.execute_sql_query(sql_query)
-                    append_to_sql_file(sql_query, output_file)
             else:
                 file_path = input('What file do you want to read it from? Please provide the absolute path. Default: datalog.txt from current directory')
                 if not file_path:
@@ -119,7 +95,3 @@ if __name__ == "__main__":
             traceback.print_exc()
             if not isREPL:
                 sys.exit(1)
-
-    observer.stop()
-    observer.join()
-    conn.close()
